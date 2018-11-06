f90235526c937bb5e7c5894462fc4cd650393d31
l8090100 <l8090100@gmail.com>
2018-11-06 14:21:17 +0800

    2018/11/16 1. import dyn4j library through gradle 2. [remove warming] replace compile with implementation in build.gradle

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
